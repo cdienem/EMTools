44ed1db5b07705c1693647baebb3574b71263c4a
Christian Dienemann <cdienem@users.noreply.github.com>
2017-04-18 19:03:59 +0200

    Update remove_errant_ter.py

--- remove_errant_ter.py
+++ remove_errant_ter.py
@@ -1,5 +1,7 @@
 #!/bin/python
 
+# phenix does not like TER cards in PDB files. THis script just kicks them out.
+
 import sys
 with open(sys.argv[1], "rb") as infile, open("no_TER.pdb","w+") as outfile:
 	for line in infile:
