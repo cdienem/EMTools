936153abf36bb2cc5cd53858ea778bcc66d8ab2f
cdienem <cdienem@users.noreply.github.com>
2017-04-18 18:11:55 +0200

    Update remove_errant_ter.sh

--- remove_errant_ter.sh
+++ remove_errant_ter.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+# this is rather slow
+# consider the python solution
+
 while read line; do if ! $(echo "$line" | grep -q "TER"); then echo $line >> out.pdb; fi; done < 5fz5_GAT1_TFIIE_homology.pdb
