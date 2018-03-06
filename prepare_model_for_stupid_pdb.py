6151b73bf76dd62ab882451a7d4fb35c6e8d58ca
Christian Dienemann <cdienem@users.noreply.github.com>
2018-03-06 14:59:53 +0100

    add prepare model

--- prepare_model_for_stupid_pdb.py
+++ prepare_model_for_stupid_pdb.py
@@ -1,25 +1,16 @@
 #!/bin/python
 
-
-# The PDB likes HETATM at a specific place. Also kick out TERs that are not at the end of a chain.
-# To be extended by more features like HELIX assignements etc.
 # Removes errant TERs for PDB, puts HETATOMS to the back
 
-# takes two arguments for input and output file
-
 import sys
-
 ifile = []
-
 with open(sys.argv[1], "rb") as infile:
 	for line in infile:
 		ifile.append(line)
 
 
 ofile = []
-
 hetatom = []		
-
 # Removes TERs
 for card in ifile:
 	if card[0:3] != "TER" and card[0:6] != "HETATM":
@@ -42,10 +33,8 @@ while i < len(ofile)-1:
 		terfile.append(ofile[i])
 	i = i+1
 
-
 oo = terfile + hetatom + ["END"]
 #print oo
-
-with open(sys.argv[2],"w+") as outfile:
+with open("out.pdb","w+") as outfile:
 	for line in oo:		
 		outfile.write(str(line))
